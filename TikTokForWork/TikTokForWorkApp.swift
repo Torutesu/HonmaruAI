7d116c4285b0f3e49af1f760b3c974bb3eb8727b
Anant Singhal <89266869+GithubAnant@users.noreply.github.com>
2026-07-27 21:57:11 +0530

    chore: Xcode project, app entry point and assets

---
+++ TikTokForWork/TikTokForWorkApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct TikTokForWorkApp: App {
+    @StateObject private var appState = AppState()
+
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+                .environmentObject(appState)
+        }
+    }
+}
